35babc8f4b317d5ba171fe82501898027c5d9471
ahs21c <ahs21c@gmail.com>
2026-03-26 14:00:57 +0900

    remove stats bar from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from '@/components/home/Hero'
-import StatsBar from '@/components/home/StatsBar'
 import PopularSearches from '@/components/home/PopularSearches'
 import CalculatorCards from '@/components/home/CalculatorCards'
 import BlogCards from '@/components/home/BlogCards'
@@ -10,7 +9,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <StatsBar />
       <PopularSearches />
       <CalculatorCards />
       <BlogCards />
